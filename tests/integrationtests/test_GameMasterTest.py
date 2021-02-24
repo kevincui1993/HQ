89a5024954c593b231736c401eb02c6404d23e84
Kevin Cui <kevin.cui@veeva.com>
2021-02-23 23:28:17 -0500

    added more integration tests

--- tests/integrationtests/test_GameMasterTest.py
+++ tests/integrationtests/test_GameMasterTest.py
@@ -11,7 +11,8 @@ class GameMasterTest(unittest.TestCase):
         super(GameMasterTest, self).__init__(*args, **kwargs)
         self.TEST_PORT = 8088
         self.TEST_HOST = '127.0.0.1'
-        self.gameMaster = GameMaster(self.TEST_HOST, self.TEST_PORT)
+        self.TEST_MIN_PLAYERS = 2
+        self.gameMaster = GameMaster(self.TEST_HOST, self.TEST_PORT, self.TEST_MIN_PLAYERS)
         self.gameMasterThread = threading.Thread(target = self.gameMaster.start)
         
         
@@ -22,7 +23,7 @@ class GameMasterTest(unittest.TestCase):
         self.gameMaster.stop()
         self.gameMasterThread.join()
 
-    def test_player_connection(self):
+    def atest_player_connection(self):
         # connect to plyerListener
         try:  
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
@@ -34,6 +35,44 @@ class GameMasterTest(unittest.TestCase):
         s.close()
 
         self.assertEqual(self.gameMaster.getPlayerCount(), 1)
+
+    def test_start_game_room(self):
+        players = []
+        for i in range(self.TEST_MIN_PLAYERS):
+            try:  
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((self.TEST_HOST, self.TEST_PORT))
+                players.append(s)
+            except:  
+                self.fail()
+
+        time.sleep(5)
+
+        for i in range(self.TEST_MIN_PLAYERS):
+            res = players[i].recv(1024).decode("utf-8")
+            self.assertTrue("Game started" in res)
+
+    def test_start_player_answer_question(self):
+        players = []
+        for i in range(self.TEST_MIN_PLAYERS):
+            try:  
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((self.TEST_HOST, self.TEST_PORT))
+                players.append(s)
+            except:  
+                self.fail()
+
+        time.sleep(5)
+
+        for i in range(self.TEST_MIN_PLAYERS):
+            players[i].send("A".encode())
+            players[i].send("B".encode())
+
+        time.sleep(5)
+
+        for i in range(self.TEST_MIN_PLAYERS):
+            res = players[i].recv(1024).decode("utf-8")
+            self.assertTrue("Correct" in res or "Wrong Answer" in res)
 
 if __name__ == '__main__':
     unittest.main()
