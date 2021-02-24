bee0ee58c542735ff35ce0a42b277ef9bb2e26cc
Kevin Cui <kevin.cui@veeva.com>
2021-02-23 22:35:29 -0500

    updated QuestionMaster to read in from json instead of from txt files. The advantage of this is we can now easily support dynamic answer generation for each question.

--- QuestionMaster.py
+++ QuestionMaster.py
@@ -1,6 +1,9 @@
-from logger import *
+from logger import log
 from random import randrange
+from random import shuffle
 import os
+import json
+import sys
 
 class QuestionMaster:
 
@@ -8,34 +11,59 @@ class QuestionMaster:
         self.questions = []
         if load:
             fileDir = os.path.dirname(os.path.realpath('__file__'))
-            qpath = os.path.join(fileDir, "gameData/questionsample.txt")
-            apath = os.path.join(fileDir, "gameData/answersample.txt")
-            qlines = self.readFile(qpath)
-            alines = self.readFile(apath)
-            self.load(qlines, alines)
+            self.load(self.readFile(os.path.join(fileDir, "gameData/questions.json")))
 
     def readFile(self, path):
         lines = ""
         try:
             with open(path) as f:
-                lines = f.read().splitlines()
+                lines = f.readlines()
         except:
             log(self.__class__.__name__).warning("Failed to open file {}".format(path))
 
-        return lines
+        return lines[0]
 
-    def load(self, qlines, alines):
-        if len(alines) != len(qlines):
-            log(self.__class__.__name__).warning("Failed to load question & answer mismatch")
-            return
+    def load(self, jsonStr):
+        try:
+            data = json.loads(jsonStr)
+        except:
+            log(self.__class__.__name__).warning("Unexpected error: {}".format(sys.exc_info()[0]))
+            return 
 
-        for i in range(len(qlines)):
-            formatted_qline = qlines[i].replace('\\n', '\n').replace('\\t', '\t')
-            self.questions.append((formatted_qline, alines[i]))
+        if "questions" in data:
+            for item in data["questions"]:
+                if "incorrect_answers" not in item or \
+                    "correct_answer" not in item or \
+                    "question" not in item or len(item["incorrect_answers"]) + 1 > 5:
+                    log(self.__class__.__name__).warning("Reject question {}".format(item))
+                    continue
+
+                self.questions.append((item["question"],  item["incorrect_answers"], item["correct_answer"]))
+        else:
+            log(self.__class__.__name__).warning("Failed to find quetions tag when loading the questions")
+
+    def formatQuestionText(self, questionStr, choices, ans):
+        text = ['A', 'B', 'C', 'D', 'E']
+
+        formattedQText = "{}\n".format(questionStr)
+        ansText = "A"
+        for i in range(len(choices)):
+            if choices[i] == ans:
+                ansText = text[i]
+            formattedQText += "{}. {}\t".format(text[i], choices[i])
+
+        return (formattedQText, ansText)
 
     def generateQwithA(self):
         index = randrange(len(self.questions))
-        return self.questions[index]
+
+        question, incorrect, correct = self.questions[index]
+        choices = incorrect + [correct]
+        shuffle(choices)
+
+        qText, ansText = self.formatQuestionText(question, choices, correct)
+
+        return (qText, ansText)
 
     def clear(self):
         self.questions.clear()
--- test_questionMasterTestU.py
+++ test_questionMasterTestU.py
@@ -4,38 +4,16 @@ import os
 
 class QuestionMasterTestU(unittest.TestCase):
 
-    def test_load_quesion_match_answers(self):
-        qMaster = QuestionMaster(False)
-
-        qMaster.load(["question1", "question2"], ["answer1", "answer2"])
-
-        self.assertEqual(qMaster.getQuestionsCount(), 2)
-
-    def test_load_quesion_mismatch_answers(self):
-        qMaster = QuestionMaster(False)
-
-        qMaster.load(["question1", "questions2"], ["answer1"])
-
-        self.assertEqual(qMaster.getQuestionsCount(), 0)
-
-    def test_load_answers_mismatch_quesion(self):
-        qMaster = QuestionMaster(False)
-
-        qMaster.load(["question1"], ["answer1", "answer2"])
-
-        self.assertEqual(qMaster.getQuestionsCount(), 0)
-
-
-    def test_readFile_valid_path(self):
+    def atest_readFile_valid_path(self):
         qMaster = QuestionMaster(False)
         fileDir = os.path.dirname(os.path.realpath('__file__'))
         
-        lines = qMaster.readFile(os.path.join(fileDir, "gameData/questionsample.txt"))
+        lines = qMaster.readFile(os.path.join(fileDir, "gameData/questions.json"))
 
         self.assertTrue(lines != "")
 
 
-    def test_readFile_invalid_path(self):
+    def atest_readFile_invalid_path(self):
         qMaster = QuestionMaster(False)
         fileDir = os.path.dirname(os.path.realpath('__file__'))
         
@@ -43,13 +21,85 @@ class QuestionMasterTestU(unittest.TestCase):
 
         self.assertTrue(lines == "")
     
-    def test_generateQwithA_valid_result(self):
+    def atest_generateQwithA_valid_result(self):
         qMaster = QuestionMaster()
 
         q,a = qMaster.generateQwithA()
 
         self.assertTrue(q != "")
-        self.assertTrue(a.lower() in ["a","b","c",'d'])
+        self.assertTrue(a.lower() in ["a","b","c",'d','e'])
 
+    def test_load_quesion_valid_data(self):
+        qMaster = QuestionMaster(False)
+
+        qMaster.load("{\"questions\":[{\"question\":\"Krusty is the guild master of which guild in Log Horizon?\",\"correct_answer\":\"D. D. D\",\"incorrect_answers\":[\"Silver Sword\",\"West Wind Brigade\",\"Oceanic Systems (Marine Agency)\"]}]}")
+
+        self.assertEqual(qMaster.getQuestionsCount(), 1)
+
+    def test_load_quesion_missing_questions_tag(self):
+        qMaster = QuestionMaster(False)
+
+        qMaster.load("{\"a\":[{\"question\":\"Krusty is the guild master of which guild in Log Horizon?\",\"correct_answer\":\"D. D. D\",\"incorrect_answers\":[\"Silver Sword\",\"West Wind Brigade\",\"Oceanic Systems (Marine Agency)\"]}]}")
+
+        self.assertEqual(qMaster.getQuestionsCount(), 0)
+
+
+    def test_load_quesion_missing_question_tag(self):
+        qMaster = QuestionMaster(False)
+
+        qMaster.load("{\"questions\":[{\"correct_answer\":\"D. D. D\",\"incorrect_answers\":[\"Silver Sword\",\"West Wind Brigade\",\"Oceanic Systems (Marine Agency)\"]}]}")
+
+        self.assertEqual(qMaster.getQuestionsCount(), 0)
+
+    def test_load_quesion_missing_incorrect_answers_tag(self):
+        qMaster = QuestionMaster(False)
+
+        qMaster.load("{\"questions\":[{\"question\":\"Krusty is the guild master of which guild in Log Horizon?\",\"correct_answer\":\"D. D. D\"}]}")
+
+        self.assertEqual(qMaster.getQuestionsCount(), 0)
+
+    def test_load_quesion_missing_correct_answers_tag(self):
+        qMaster = QuestionMaster(False)
+
+        qMaster.load("{\"questions\":[{\"question\":\"Krusty is the guild master of which guild in Log Horizon?\",\"incorrect_answers\":[\"Silver Sword\",\"West Wind Brigade\",\"Oceanic Systems (Marine Agency)\"]}]}")
+
+        self.assertEqual(qMaster.getQuestionsCount(), 0)
+
+    def test_load_quesion_malform_json(self):
+        qMaster = QuestionMaster(False)
+
+        qMaster.load("{\"questions\":[{\"question\":\"Krusty is the guild master of which guild in Log Horizon?\",\"correct_answer\":\"D. D. D\",\"incorrect_answers\":[\"Silver Sword\",\"West Wind Brigade\",\"Oceanic Systems (Marine Agency)\"]")
+
+        self.assertEqual(qMaster.getQuestionsCount(), 0)
+
+    def test_formatQuestionText_multiple_choices(self):
+        qMaster = QuestionMaster(False)
+        questionStr, choices, ans = "question?", ["ans1", "ans2", "ans3", "ans4"], "ans4"
+
+        formattedQText, ansText = qMaster.formatQuestionText(questionStr, choices, ans)
+
+        self.assertTrue("question?" in formattedQText)
+        self.assertTrue("A. " in formattedQText)
+        self.assertTrue("\tB. " in formattedQText)
+        self.assertTrue("\tC. " in formattedQText)
+        self.assertTrue("\tD. " in formattedQText)
+        self.assertTrue("\tE. " not in formattedQText)
+        self.assertEquals(formattedQText[formattedQText.find(ans)-3], ansText)
+
+    def test_formatQuestionText_true_false(self):
+        qMaster = QuestionMaster(False)
+        questionStr, choices, ans = "question?", ["true","false"], "true"
+
+        formattedQText, ansText = qMaster.formatQuestionText(questionStr, choices, ans)
+
+        self.assertTrue("question?" in formattedQText)
+        self.assertTrue("A. " in formattedQText)
+        self.assertTrue("\tB. " in formattedQText)
+        self.assertTrue("\tC. " not in formattedQText)
+        self.assertTrue("\tD. " not in formattedQText)
+        self.assertTrue("\tE. " not in formattedQText)
+        self.assertEquals(formattedQText[formattedQText.find(ans)-3], ansText)
+
+        
 if __name__ == '__main__':
     unittest.main()
